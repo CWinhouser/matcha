652297dcb72242e28eb39396dcaf2ab6e44d7853
BuildTools <kyletwomey99@gmail.com>
2020-09-09 15:12:02 +0200

    removing oauth token

--- functions/sendMail.js
+++ functions/sendMail.js
@@ -10,11 +10,11 @@ let transporter = mailer.createTransport({
 	port: 465,
 	auth: {
 		type: 'OAuth2',
-		user: 'matchwtc@gmail.com',
+		user: '',
 		clientId: '311779668400-4us7aps25ngnd5sdghdqu6plejs1mf0l.apps.googleusercontent.com',
 		clientSecret: 'Da9DOV1PBJjLTrPGXucAN52M',
-		refreshToken: '1//04WkYpV-YEEEKCgYIARAAGAQSNwF-L9IrJ2j8GfLPF9pChQtiNyg6ukV_GG3ozgBr8gUiikr0BaOqAYctgbHnkF--_s1ekJCho2k',
-		accessToken: 'ya29.a0AfH6SMAg5cs9dUuklcdRWiLndh5Qq_8LsIm3Builamrt3fO4dyRulsqhy-0KOMqqM_cVzO0YrElh1hnm9Ek2Gw-0QfnKq_43wNbXs804xig37znq4huK2Og-l8ih5PMd6_fP4z1fIAqrOLO6hTHy1pHnAjJQQ9fkD9U'
+		refreshToken: '',
+		accessToken: ''
 	}
 });
 
